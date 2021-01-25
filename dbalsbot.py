4568d58262d5b15f3617d59adc2ec7fc2ca01279
dbalskim <70954150+dbalskim@users.noreply.github.com>
2021-01-25 19:20:38 +0900

    Update dbalsbot.py

--- dbalsbot.py
+++ dbalsbot.py
@@ -1,6 +1,6 @@
 import discord
 import random
-#import os
+import os
 import requests
 from bs4 import BeautifulSoup
 
@@ -582,7 +582,5 @@ async def on_message(message):
 
 
 
-#access_token = os.environ['BOT_TOKEN']
-#client.run(access_token)
-
-client.run("ODAxNzM4OTE5NjEwODEwMzY5.YAlDjg.T-D8Ttw509GPm_D31nyQnkifffM")
+access_token = os.environ['BOT_TOKEN']
+client.run(access_token)
